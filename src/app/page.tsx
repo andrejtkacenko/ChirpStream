d5aff9621306e70b3cccefaa91afec1b50552366
Алексей <a79347304@gmail.com>
2025-08-31 15:29:03 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <HomePageContent>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,9 +53,6 @@ function HomePageContent() {
 
   return (
       <main>
-        <div className="p-4 border-b flex items-center gap-4 sticky top-0 bg-background/80 backdrop-blur-sm z-10">
-          <h1 className="text-xl font-bold">Home</h1>
-        </div>
         <CreatePostForm onPostCreated={loadFeed} />
         {feedLoading ? <FeedSkeleton /> : (
           <div className="flex flex-col">
